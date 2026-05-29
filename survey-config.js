84ef4904a93c127fdf286e993f20c4e4efa81250
seewside <seewside@gmail.com>
2026-05-29 16:17:39 +0900

    Configure survey Apps Script endpoint

--- survey-config.js
+++ survey-config.js
@@ -1,3 +1,3 @@
 // Apps Script 배포 후 발급된 웹 앱 URL을 여기에 넣으세요.
 // 예: "https://script.google.com/macros/s/AKfycb.../exec"
-window.CAKEY_SURVEY_WEB_APP_URL = "";
+window.CAKEY_SURVEY_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyUM5B_SC2qrIlFY-lXhv0gfZFChn06ApjG7ErW44orwzI_D4w3KJroL4x8xWn8AcsQ/exec";
